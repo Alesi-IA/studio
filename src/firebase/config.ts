d7a6913052667a3e8f80f63054091b38644f7861
Alexis Busto <alexisg.busto@gmail.com>
2025-10-22 21:58:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7324678641-463b8",
+  "appId": "1:1058142614465:web:6edbac373376b420cdf540",
+  "apiKey": "AIzaSyC635786zZMQLcPywoEeCsS78DAhX0t_S4",
+  "authDomain": "studio-7324678641-463b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1058142614465"
+};
