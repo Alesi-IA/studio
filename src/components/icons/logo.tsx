ee4bcd645f0134eb1ef65b7039dba32abdb47a87
Alexis Busto <alexisg.busto@gmail.com>
2025-10-26 00:12:58 +0000

    Podemos poner como logo del proyecto, la imagen que tenemos en este chat

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,12 +1,12 @@
-import { CannabisLeafIcon } from "./cannabis-leaf";
+import { TowlieIcon } from "./towlie";
 import { cn } from "@/lib/utils";
 
 export function CannaGrowLogo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-3", className)}>
-      <div className="relative flex h-8 w-8 items-center justify-center rounded-lg bg-primary/20 text-primary-foreground">
-        <div className="absolute inset-0 rounded-lg bg-primary opacity-80"></div>
-        <CannabisLeafIcon className="relative h-5 w-5" />
+      <div className="relative flex h-9 w-9 items-center justify-center rounded-lg bg-blue-400/20 text-primary-foreground">
+        <div className="absolute inset-0 rounded-lg bg-blue-400 opacity-80"></div>
+        <TowlieIcon className="relative h-7 w-7" />
       </div>
     </div>
   );
