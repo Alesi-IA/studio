c88716438a6cbdeb5c22954c616be2355901a300
Alexis Busto <alexisg.busto@gmail.com>
2025-10-27 20:30:14 +0000

    Revisa si están bien escritas y puestas o si me faltan algún símbolo de

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,13 @@
 // 4. Selecciona la opción "SDK setup and configuration" y elige "Config".
 // 5. Copia el objeto de configuración y pégalo aquí, reemplazando el objeto de ejemplo.
 
+// EJEMPLO DE CÓMO DEBERÍA VERSE CON CLAVES FICTICIAS:
 export const firebaseConfig = {
-  apiKey: "<TU_API_KEY_DE_FIREBASE_AQUI>",
-  authDomain: "<TU_AUTH_DOMAIN_AQUI>",
-  projectId: "<TU_PROJECT_ID_AQUI>",
-  storageBucket: "<TU_STORAGE_BUCKET_AQUI>",
-  messagingSenderId: "<TU_MESSAGING_SENDER_ID_AQUI>",
-  appId: "<TU_APP_ID_AQUI>",
-  measurementId: "<TU_MEASUREMENT_ID_OPCIONAL_AQUI>"
+  apiKey: "AIzaSyC0s9umzyIGVi3yCPpaKCM7stWyEXAMPLE",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890abcdef",
+  measurementId: "G-ABCDEFGHIJ"
 };
